add94b9cf35c17a672e6bd08a541ca65855bdafc
Christopher Nelson <nadiasvertex@gmail.com>
2010-11-28 08:10:34 -0500

    Compiles again, added explicit location for singletons.

--- src/core/singletons.h
+++ src/core/singletons.h
@@ -9,6 +9,24 @@ namespace proton {
 /// The one and only None object.
 object* object::none = new object(type::none);
 
+type* type::none = new type(L"NoneType");
+
+type* type::list = new type(L"list");
+type* type::dict = new type(L"dict");
+type* type::set = new type(L"set");
+type* type::tuple = new type(L"tuple");
+
+type* type::function = new type(L"function");
+
+type* type::py_int = new type(L"int");
+type* type::py_bool = new type(L"bool");
+type* type::py_float = new type(L"float");
+type* type::py_complex = new type(L"complex");
+type* type::py_str = new type(L"str");
+
+type* type::py_type = new type(L"type");
+type* type::py_namespace = new type(L"namespace");
+
 }
 
 #endif /* SINGLETONS_H_ */
